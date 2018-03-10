ab8d952ad6b6ef51b05f2d80d4ba011c64e1706e
Peter Seremetis <pswriter5348@gmail.com>
2018-03-10 21:35:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ function setCart(c) {
 
 function addToCart(item) {
   var itemObject = {}
-              var itemPrice: priceMath.random }
   for(i = 0; i < cart.length; i++){
   return itemObject.push(`CABBAGES`);
 }
