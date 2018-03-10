3cf555e0013903a4c929a3118d8b70439fd696a4
Peter Seremetis <pswriter5348@gmail.com>
2018-03-10 20:58:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,18 @@ function setCart(c) {
 
 function addToCart(item) {
   var cart = [];
-  var obj = {}
-  return item.push(`${item}`)
+  var obj = {`${item}`: Math.random}
+  var object = new Object ([value]);
+  if (cart.length = 0){
+    return (`Your shopping cart is empty.`)
+  }else{
+    let i = 0;
+    while(i < cart.length){
+    cart.push(`${obj}`);
+    i++
+  }
+  return cart
+ }
 }
  //return [{ item, price }, { }, { }]
  //generate random math integer for price
